307859c4c9779304f03cbe8ec7b277872846652f
khsrhhh <khsrhhh@gmail.com>
2018-12-04 19:04:40 +0900

    set threshold

--- lane_detection.py
+++ lane_detection.py
@@ -1,5 +1,18 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('road.jpg')
-cv2.imshow('result', image)
+np_image = np.copy(image)   #copy image to numpy format
+
+blue_threshold = 200
+red_threshold = 200
+green_threshold = 200
+bgr_threshold = [blue_threshold, green_threshold, red_threshold]
+
+thresholds = (image[:,:,0] < bgr_threshold[0]) | (image[:,:,1] < bgr_threshold[1]) | (image[:,:,2] < bgr_threshold[2])
+np_image[thresholds] = [0, 0, 0]
+
+cv2.imshow('white', np_image)
+cv2.imshow('result',image)
 cv2.waitKey(0)
+
