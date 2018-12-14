a4a66b4c4d4de92359cd2e3d86d02f906321e008
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-12-14 21:23:13 +0900

    add hough filter function

--- lane_detection_edge.py
+++ lane_detection_edge.py
@@ -10,6 +10,15 @@ def canny(img, low_threshold, high_threshold):
 def gaussian_blur(img, kernel_size):
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0) # what is sigmaX?
 
+#not finish
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):    # hough filter...
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    
+    return line_img
+#not finish
+
 image = cv2.imread('road1.jpg')
 height, width = image.shape[:2]
 
